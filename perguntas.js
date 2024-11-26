77522dc26b98ef6f3af2d2219193454acf0d8f6f
kkjJoao7 <joaodanielbarretoribeiro7@gmail.com>
2024-11-26 13:59:54 -0300

    Add files via upload
    adicionando o projeto

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que faz um programador?',
+    'Esse profissional cria aplicativos, programas, sistemas, sites e até games utilizando diversas linguagens de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é uma placa tectônica?',
+    'Placas tectônicas são blocos semirrígidos que formam a crosta terrestre'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um body na programacao?',
+    'O elemento <body> do HTML representa o conteúdo de um documento HTML'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual a forma correta do verbo to be na primeira pessoa do singular?',
+    '“Am” sempre é usado para a primeira pessoa do singular, ou seja, ele é usado com “I”'
+)
